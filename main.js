31e235293e28dbdf95b9adec15002ae7d047d44f
simpego81 <pegoraro.simone.1981@gmail.com>
2016-12-15 14:50:19 +0100

    correzione bug sull'init del server

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 
+// setup ports
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
 app.get('/', function(req, res) {
 	res.end('Pagina principale!');
 });
