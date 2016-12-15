a7130dd534f6ded5649a5439843149b55e1e814b
simpego81 <pegoraro.simone.1981@gmail.com>
2016-12-15 14:17:07 +0100

    copia del tutorial

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+	res.end('Pagina principale!');
+});
+
+// server listens in on port
+app.listen(server_port, server_ip_address, function () {
+	 console.log( "...Listening on " + server_ip_address + ", server_port " + server_port );
+});
+
 console.log("Applicazione: tutto OK!");
