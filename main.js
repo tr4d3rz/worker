d7894dc24d00930deb993c9cbac31a28e39f9bd6
simpego81 <pegoraro.simone.1981@gmail.com>
2016-12-19 21:27:31 +0100

    correzione su main.js

--- main.js
+++ main.js
@@ -7,10 +7,9 @@ app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  response.send('index.html');
 });
 
 app.listen(app.get('port'), function() {
